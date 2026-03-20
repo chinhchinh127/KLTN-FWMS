9ba47bb3c736e5f37ce3a973daf55175530d871e
Hung <hungle280204@gmail.com>
2026-03-20 08:11:48 +0700

    update login

---
+++ src/components/PrivateRoute.jsx
@@ -0,0 +1,8 @@
+import { Navigate } from "react-router-dom";
+
+const PrivateRoute = ({ children }) => {
+    const isLogin = localStorage.getItem("token");
+    return isLogin ? children : <Navigate to="/" />;
+};
+
+export default PrivateRoute;
