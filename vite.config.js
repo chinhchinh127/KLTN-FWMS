f179f6a4031c743809ed29a354b0c3311f9ea8ef
Chau Chinh <chauchinh1272004@gmail.com>
2026-03-21 14:35:10 +0700

    Fix base path cho Vercel

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
     plugins: [react()],
-    base: "/KLTN-FWMS/",
+    // base: "/KLTN-FWMS/",
+    base: "/",
     server: {
         port: 3000,
         open: true,
